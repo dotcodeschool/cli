beaee27e5aa27b03e35cf84ad9acec1c0fa23bee
iammasterbrucewayne <batman@dotcodeschool.com>
2024-10-03 17:19:49 +0530

    fix(src/constants.rs): replace localhost BACKEND_URL with prod url

--- src/constants.rs
+++ src/constants.rs
@@ -1,3 +1,2 @@
-// pub const BACKEND_URL: &str = "https://backend.dotcodeschool.com/api/v0";
-pub const BACKEND_URL: &str = "http://localhost:8080/api/v0";
+pub const BACKEND_URL: &str = "https://backend.dotcodeschool.com/api/v0";
 pub const LOG: &str = "./dcs.log";
